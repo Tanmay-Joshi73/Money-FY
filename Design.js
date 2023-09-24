a56afff0691aa4bbb3ffca144851dc1abff743ad
unknown <tanmayjoshi072@gmail.com>
2023-09-24 12:36:29 -0700

    Hyperlinks Just Added To The Header Li

--- Design.js
+++ Design.js
@@ -1,4 +1,5 @@
-
+const SignUp_Header=document.getElementById('Header-SignUP')
+const Login_Header=document.getElementById('Header-Login')
 const SignUp_Block = document.getElementById('SignUp');
 const Login_Block = document.getElementById('Login');
 const Login=document.getElementById('Toogle_Login')
@@ -11,4 +12,12 @@ Login.addEventListener('click',()=>{
 SignUp.addEventListener('click',()=>{
     Login_Block.style.display='none'
     SignUp_Block.style.display='block'
+})
+SignUp_Header.addEventListener('click',()=>{
+    Login_Block.style.display='none'
+    SignUp_Block.style.display='block'
+})
+Login_Header.addEventListener('click',()=>{
+    Login_Block.style.display='block'
+    SignUp_Block.style.display='none'
 })
